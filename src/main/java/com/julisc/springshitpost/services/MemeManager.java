c3ef393c173a7ac07072e1d3def7e93c49ce043a
JuliSC <julius@adnudging.com>
2020-10-17 16:41:17 +0200

    Creates file now

--- src/main/java/com/julisc/springshitpost/services/MemeManager.java
+++ src/main/java/com/julisc/springshitpost/services/MemeManager.java
@@ -10,13 +10,13 @@ import java.util.Scanner;
 
 public class MemeManager {
 
-    private File garbageDump = new File("src/main/resources/static/assets/files/actual-fucking-garbage.txt");
-
     public MemeManager(){
 
     }
 
     public void dumpTheLoad(Meme load) throws IOException {
+        File garbageDump = new File("src/main/resources/static/assets/files/actual-fucking-garbage.txt");
+
         PrintWriter garbageTruck = new PrintWriter(new FileWriter(garbageDump, true));
         garbageTruck.println(load.getMeme());
         garbageTruck.close();
@@ -24,6 +24,12 @@ public class MemeManager {
 
     public List<Meme> scavengeForShitposts() {
         try {
+            File garbageDump = new File("src/main/resources/static/assets/files/actual-fucking-garbage.txt");
+            if(!garbageDump.exists()){
+                garbageDump.getParentFile().mkdirs();
+                garbageDump.createNewFile();
+            }
+
             List<Meme> shitpostBackpack = new ArrayList<>();
             Scanner shitpostFinder = new Scanner(garbageDump);
             while(shitpostFinder.hasNextLine()) {
